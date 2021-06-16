3aaf24679cdac0ede75e2a4ed07cba07311c3459
Zak <slocumz@ferris.edu>
2021-06-16 09:35:43 -0400

    added ability for customer to choose crust

---
+++ PizzaCrust.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PizzaCrust {
+	int crustPref;
+	int userInput;
+	
+	// Allows customer to choose their style of crust
+	private void chooseCrust() {
+		Scanner scnr = new Scanner(System.in);
+		System.out.println("How would you like your crust? \n 1. Soft\n 2. Medium\n 3. Hard");
+		userInput = scnr.nextInt();
+		
+		if(userInput == 1) {
+			System.out.println("Softer crust, understood.");
+			crustPref = userInput;
+		}
+		else if(userInput == 2) {
+			System.out.println("Medium crust, you got it.");
+			crustPref = userInput;
+		}
+		else if(userInput == 3) {
+			System.out.println("Hard crust, nice and crunchy.");
+			crustPref = userInput;
+		}
+		else {
+			System.out.println("Not a valid input.");
+		}
+		
+		
+	}
+}
