a51599f57e3a06d206cd8d8d7cd18dcfb095a568
glizondo <80534057+glizondo@users.noreply.github.com>
2022-06-09 10:41:55 -0400

    Correcting error

--- PizzaCrust.java
+++ PizzaCrust.java
@@ -5,7 +5,8 @@ public class PizzaCrust {
 	int userInput;// added a comment
 	
 	// Allows customer to choose their style of crust
-		private void chooseCrust() {
+	// Adding comment to test conflict
+	private void chooseCrust() {
 		Scanner scnr = new Scanner(System.in);
 		System.out.println("How would you like your crust? \n 1. Soft\n 2. Medium\n 3. Hard");
 		userInput = scnr.nextInt();
