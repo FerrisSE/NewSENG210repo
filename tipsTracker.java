d5ba13f1dff456434be420fb0c1dc597b2d65558
PlanetPunkCK <nims.pc57@gmail.com>
2023-06-14 11:45:42 -0400

    added comment for tip calculations

---
+++ tipsTracker.java
@@ -0,0 +1,12 @@
+
+
+// comment 1
+
+public class tipsTracker {
+	private float tips;
+	public void printTips() {
+		System.out.println("Your current tip balance is: " + tips);
+	}
+}
+
+// comment 2 add tip calculator
