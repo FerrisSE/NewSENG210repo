38e7563b45a6c1e7ce384c344481e03cf89a8bb9
PlanetPunkCK <nims.pc57@gmail.com>
2023-06-14 11:50:40 -0400

    tipsCalc function method added

--- tipsTracker.java
+++ tipsTracker.java
@@ -1,12 +1,15 @@
-
-
 // comment 1
-
 public class tipsTracker {
-	private float tips;
+	public float tips;
+	public float newTips;
 	public void printTips() {
 		System.out.println("Your current tip balance is: " + tips);
 	}
-}
+
 
 // comment 2 add tip calculator
+	public float tipsCalc(float tips, float newTip) {
+		tips += newTip;  // total tip calculation
+		return tips;  // return current tip balance
+	}
+}
