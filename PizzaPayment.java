d6f5520dc8e4a124fdd90cb3c916a7cd308718f7
Jesus Aguilar <jesusaguilar92348@gmail.com>
2024-06-12 12:50:32 -0400

    Futher Developed the not working PizzaPayment Class

--- PizzaPayment.java
+++ PizzaPayment.java
@@ -1,10 +1,37 @@
 //Calculate payment for Pizzas
 
-import someCodeHere;
-import someCodeThere;
+public class PizzaPayment {
 
-private void calcPrice() {
-	numPizza = 1;
-	price = numPizza * 5;
-	custPayment = price - payment;
+    private double[] pizzaPrices;
+    private double payment;
+    private double totalCost;
+
+    public PizzaPayment(double[] pizzaPrices, double payment) {
+        this.pizzaPrices = pizzaPrices;
+        this.payment = payment;
+        this.totalCost = calculateTotalCost();
+    }
+
+    private double calculateTotalCost() {
+        double cost = 0;
+        for (double price : pizzaPrices) {
+            cost += price;
+        }
+        cost += (cost * 0.06); // Adding Taxes (6%)
+        return cost;
+    }
+
+    public double TotalCost() {
+        return totalCost;
+    }
+
+    public double processPayment() {
+        return payment - totalCost;
+    }
+
+    @Override
+    public String toString() {
+        return "The total cost of the order is $" + totalCost + ", after paying $" + payment + " your total balance is $" + processPayment() + ".";
+    }
 }
+
