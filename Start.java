9c0a8306e306f26fb64e5af90987545e921dd294
Jackson Monson <20jackson03@gmail.com>
2024-06-14 12:31:02 -0400

    User input prompt now in a loop

--- Start.java
+++ Start.java
@@ -4,28 +4,32 @@ public class Start {
 	public static void main(String[] args) {
 		//objects
 		Scanner scnr = new Scanner(System.in);
+		boolean validAnswer = false;
 		
 		//variables
 		int userInput;
 		
-		
-		System.out.println("Welcome to Joey's Pizza! What can we make for you?");
-		System.out.println("Type a number to choose:\n 1.Pizza \n 2.Pasta \n 3.Subs");
-		userInput = scnr.nextInt();
-		
-		//FIXME add what happens when customer selects an option
-		if (userInput == 1) {
-			System.out.println("One pizza coming up!");
-		}
-		else if (userInput == 2) {
-			System.out.println("One pasta coming up!");
-		}
-		else if (userInput == 3) {
-			System.out.println("One sub coming up!");
-		}
-		else {
-			System.out.println("Invalid answer. ");
+		while(validAnswer == false) {
+			System.out.println("Welcome to Joey's Pizza! What can we make for you?");
+			System.out.println("Type a number to choose:\n 1.Pizza \n 2.Pasta \n 3.Subs");
+			userInput = scnr.nextInt();
+			
+			validAnswer = true;
+			
+			//add what happens when customer selects an option
+			if (userInput == 1) {
+				System.out.println("One pizza coming up!");
+			}
+			else if (userInput == 2) {
+				System.out.println("One pasta coming up!");
+			}
+			else if (userInput == 3) {
+				System.out.println("One sub coming up!");
+			}
+			else {
+				System.out.println("Invalid answer, please try again. ");
+				validAnswer = false;
+			}
 		}
 	}
-
 }
