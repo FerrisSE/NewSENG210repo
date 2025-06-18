a694ef940539c987d6efd6505fd1e31a2dda3fc0
Ethan-Witter-Student <112420827+EthanWitterStudent@users.noreply.github.com>
2025-06-18 14:05:04 -0400

    Rewrote Pizza Toppings to be functional

--- Pizza Toppings.java
+++ Pizza Toppings.java
@@ -1,6 +1,53 @@
+
+
+class PizzaToppings
+{
+	//the default is a boring cheese pizza
+	//Also its obvious but: true if its on, false if its off.
+	public boolean pepperoni = false;
+	public boolean bacon = false;
+	public boolean stuffedCrust = false;
+	public boolean pineapple = false;
+	public boolean mushrooms = false;
+	
+	
+	PizzaToppings(boolean desiredPepperoni, boolean desiredBacon, boolean desiredStuffedCrust, 
+				boolean desiredPineapple, boolean desiredMushrooms)
+	{
+		//simply set the toppings as required
+		this.pepperoni = desiredPepperoni;
+		this.bacon = desiredBacon;
+		this.stuffedCrust = desiredStuffedCrust;
+		this.pineapple = desiredPineapple;
+		this.mushrooms = desiredMushrooms;
+		
+		//Now, if the desired toppings need to be known, they can do PizzaToppings.[whatever]
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//EthanZW here: left this as an archive
 //Choosing toppings
 
-import pizzaToppings;
+/*import pizzaToppings;
 //I'm not sure what this is...
 
 PT {
@@ -9,4 +56,5 @@ PT {
 	stuffedCrust = true;
 	pineapple = false;
 	mushrooms = true;
-}
+}*/
+
